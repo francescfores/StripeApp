545a3020a4ca36e78586abe9a25abc2dee1400b1
Francesc Fores <francescfores@iesebre.com>
2016-02-06 20:58:25 +0100

    Add layouts and styles

--- app/src/main/java/sensors/francesc/dam2/iesebre/com/stripeapp/MainActivity.java
+++ app/src/main/java/sensors/francesc/dam2/iesebre/com/stripeapp/MainActivity.java
@@ -9,12 +9,27 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.stripe.android.Stripe;
+import com.stripe.android.TokenCallback;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
+
+import sensors.francesc.dam2.iesebre.com.stripeapp.dialog.ProgressDialogFragment;
+
+
+public static final String PUBLISHABLE_KEY = "pk_test_6pRNASCoBOKtIshFeQd4XMUh";
+private ProgressDialogFragment progressFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressFragment = ProgressDialogFragment.newInstance(R.string.progressMessage);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +41,58 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void saveCreditCard(PaymentForm form) {
+
+        Card card = new Card(
+                form.getCardNumber(),
+                form.getExpMonth(),
+                form.getExpYear(),
+                form.getCvc());
+        card.setCurrency(form.getCurrency());
+
+        boolean validation = card.validateCard();
+        if (validation) {
+            startProgress();
+            new Stripe().createToken(
+                    card,
+                    PUBLISHABLE_KEY,
+                    new TokenCallback() {
+                        public void onSuccess(Token token) {
+                            getTokenList().addToList(token);
+                            finishProgress();
+                        }
+                        public void onError(Exception error) {
+                            handleError(error.getLocalizedMessage());
+                            finishProgress();
+                        }
+                    });
+        } else if (!card.validateNumber()) {
+            handleError("The card number that you entered is invalid");
+        } else if (!card.validateExpiryDate()) {
+            handleError("The expiration date that you entered is invalid");
+        } else if (!card.validateCVC()) {
+            handleError("The CVC code that you entered is invalid");
+        } else {
+            handleError("The card details that you entered are invalid");
+        }
+    }
+
+    private void startProgress() {
+        progressFragment.show(getSupportFragmentManager(), "progress");
+    }
+
+    private void finishProgress() {
+        progressFragment.dismiss();
+    }
+
+    private void handleError(String error) {
+        DialogFragment fragment = ErrorDialogFragment.newInstance(R.string.validationErrors, error);
+        fragment.show(getSupportFragmentManager(), "error");
+    }
+
+    private TokenList getTokenList() {
+        return (TokenList)(getSupportFragmentManager().findFragmentById(R.id.token_list));
     }
 
     @Override
--- app/src/main/java/sensors/francesc/dam2/iesebre/com/stripeapp/dialog/ErrorDialogFragment.java
+++ app/src/main/java/sensors/francesc/dam2/iesebre/com/stripeapp/dialog/ErrorDialogFragment.java
@@ -1,7 +1,47 @@
 package sensors.francesc.dam2.iesebre.com.stripeapp.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import sensors.francesc.dam2.iesebre.com.stripeapp.R;
+
 /**
  * Created by francesc on 06/02/16.
  */
 public class ErrorDialogFragment {
+
+    public static ErrorDialogFragment newInstance(int titleId, String message) {
+        ErrorDialogFragment fragment = new ErrorDialogFragment();
+
+        Bundle args = new Bundle();
+        args.putInt("titleId", titleId);
+        args.putString("message", message);
+
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    public ErrorDialogFragment() {
+        // Empty constructor required for DialogFragment
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        int titleId = getArguments().getInt("titleId");
+        String message = getArguments().getString("message");
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(titleId)
+                .setMessage(message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create();
+    }
 }
